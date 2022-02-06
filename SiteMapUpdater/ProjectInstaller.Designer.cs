14419fd42ea6013fab6257d2305086f3d52c1566
Captain Renegade <Captain Renegade@INTELNUC>
2022-02-06 08:07:24 -0500

    Add project files.

---
+++ SiteMapUpdater/ProjectInstaller.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace SiteMapUpdater
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.SiteMapUpdater = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // SiteMapUpdater
+            // 
+            this.SiteMapUpdater.Description = "This utility updates the timestamp in the sitemap.xml file for configured sites.";
+            this.SiteMapUpdater.DisplayName = "SiteMapUpdater";
+            this.SiteMapUpdater.ServiceName = "SiteMapUpdater";
+            this.SiteMapUpdater.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.SiteMapUpdater});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller SiteMapUpdater;
+    }
+}
---
+++ SiteMapUpdater/SiteMapUpdater.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.ServiceProcess;
+using System.IO;
+using System.Xml;
+using System.Net;
+using System.Timers;
+
+namespace SiteMapUpdater
+{
+    public partial class SiteMapUpdater : ServiceBase
+    {
+
+        static string path = Environment.CurrentDirectory + "\\settings.config";
+        string sPath = "";
+        string s2Path = "";
+        string tmp = "";
+        string NewsPath = "";
+
+        AESEncryption AES = new AESEncryption();
+
+        public SiteMapUpdater()
+        {
+            InitializeComponent();
+        }
+        
+        Timer timer1 = new Timer();
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.Interval = 24 * 3600000; //every 1 day
+            timer1.Elapsed += new ElapsedEventHandler(daily_update);
+            timer1.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+        }
+
+        private void daily_update(object sender, EventArgs e)
+        {
+            if (File.Exists(path))
+            {               
+                foreach (string line in File.ReadAllLines(path))
+                {
+                    sPath = AES.Decrypt(line) + "\\sitemap.xml";
+                    s2Path = AES.Decrypt(line) + "\\sitemap2.xml";
+                    tmp = AES.Decrypt(line) + "\\tmpfile.tmp";
+                    NewsPath = AES.Decrypt(line) + "\\sitemap.xml";
+
+                    if (File.Exists(sPath))
+                    {
+                        WriteSiteMapXML();
+                    }
+                   
+                }
+            }          
+        }
+
+        //*********START DAILY UPDATE****************
+        public void WriteSiteMapXML()
+        {
+            if (!File.Exists(s2Path))
+            {
+                StreamWriter sw = File.CreateText(s2Path);
+                {
+                    for (int i = 0; i <= GetSiteMapXMLStream().Length - 1; i++)
+                    {
+                        sw.WriteLine(GetSiteMapXMLStream().GetValue(i).ToString());
+                    }
+
+                    DataSet ds = GetSiteMapXML();
+
+
+                    for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+                    {
+                        ds.Tables[0].Rows[i][1] = DateTime.Now.ToString("s") + "+00:00";
+                    }
+
+
+                    sw.WriteLine("");
+                    sw.WriteLine("");
+                    sw.Close();
+
+
+                    FileStream fappend = File.Open(tmp, FileMode.Append);
+                    {
+                        foreach (DataTable dtt in ds.Tables)
+                        {
+                            dtt.WriteXml(fappend);
+                        }
+                        fappend.Close();
+
+                    }
+
+
+                    FileStream XMLAppend = File.Open(s2Path, FileMode.Append);
+                    {
+                        StreamWriter append = new StreamWriter(XMLAppend);
+                        {
+                            int i = 0;
+                            foreach (string line in GetSiteMapTMPStream(tmp))
+                            {
+                                if (i != 0 && i != GetSiteMapTMPStream(tmp).Length)
+                                {
+                                    append.WriteLine(line);
+                                }
+                                i++;
+                            }
+                            append.Close();
+                        }
+
+                    }
+
+                }
+
+
+            }
+
+            File.Delete(tmp);
+            File.Delete(sPath);
+            File.Copy(s2Path, NewsPath);
+            File.Delete(s2Path);
+        }
+        //*********END DAILY UPDATE****************
+
+
+        //*********REFERENCED FUNCTIONS DEFINITIONS****************
+        public Array GetSiteMapXMLStream()
+        {
+            List<string> Lines = new List<string>();
+            StreamReader sr = File.OpenText(sPath);
+            {
+                string line;
+                line = sr.ReadLine();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    Lines.Add(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+
+            return Lines.ToArray();
+        }
+
+        public Array GetSiteMapTMPStream(string tmpfile)
+        {
+            List<string> Lines = new List<string>();
+            StreamReader sr = File.OpenText(tmpfile);
+            {
+                string line;
+                line = sr.ReadLine();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    Lines.Add(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+
+            return Lines.ToArray();
+
+        }
+
+        public DataSet GetSiteMapXML()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+
+            XmlReader XML = XmlReader.Create(sPath, new XmlReaderSettings());
+            DataSet ds = new DataSet();
+            ds.ReadXml(XML);
+            XML.Close();
+
+            return ds;
+        }
+
+
+
+    }
+}
